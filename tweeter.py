c878480227f061684ed096b7f5f146320982992d
Zercha <david.maskoo@gmail.com>
2015-10-10 12:25:21 +0200

    added ngram
    Signed-off-by: Zercha <david.maskoo@gmail.com>

--- tweeter.py
+++ tweeter.py
@@ -14,12 +14,6 @@ APP_SECRET = 'DaNCLGNlw1sOuZrlBUTGoRPW99pH3vlkhQuxOTJXUherwPwtZY'
 ACCESS_TOKEN = '335495642-WkhVdvjhipAWAbbqHrK0aWcOY7fIUC1ARynhEbit'
 ACCESS_SECRET = 'dzo6CLq2JY6WNSooINBEl0yH5AYpdHcYq12Y2CXKALN7I'
 
-main()
-
-def main():
-	for tweet in get_tweets('marcus', 200):
-		print(tweet)
-
 def get_tweets(hashtag, tweetCount):
 	twitter = Twython(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 	results = twitter.search(q=hashtag, count=tweetCount)
@@ -31,3 +25,7 @@ def get_tweets(hashtag, tweetCount):
 	        text = re.sub(':|\\n', '', text)
 	        tweets.append(text)
 	return tweets
+
+if __name__ == '__main__':
+	for tweet in get_tweets('marcus', 200):
+		print(tweet)
