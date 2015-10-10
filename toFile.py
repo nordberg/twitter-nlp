827654627c73eab39367ae5e101e9790888e7ad2
Simon Karlsson <simonkarlsson@msn.com>
2015-10-10 16:41:33 +0200

    Newline

--- toFile.py
+++ toFile.py
@@ -5,6 +5,5 @@ if __name__ == '__main__':
 	numberOfTweets = 2000
 	f = open('tweet_' + hashtag,'w')
 	for tweet in tweeter.get_tweets(hashtag, numberOfTweets)[0]:
-		f.write(tweet)
-		f.write('\n')
+		f.write(tweet + '\n')
 	f.close()
