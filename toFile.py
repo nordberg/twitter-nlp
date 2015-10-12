3176fdf080c68a221258e82c16e88bed767bff52
feliciar <felicia@rosells.se>
2015-10-12 13:46:14 +0200

    Changed so that it appends the collected rows to the existing file, instead of replacing the file

--- toFile.py
+++ toFile.py
@@ -1,9 +1,18 @@
 import tweeter
+import os.path
 
 if __name__ == '__main__':
-	hashtag = "obama"
+	hashtag = "test"
 	numberOfTweets = 2000
-	f = open('tweet_' + hashtag,'w')
+	
+	
+	
+	if os.path.isfile('tweet_' + hashtag):
+		f = open('tweet_' + hashtag,'a') #append to file
+	else :
+		f = open('tweet_' + hashtag,'w') #create new file and write to
+	
+
 	for tweet in tweeter.get_tweets(hashtag, numberOfTweets)[0]:
 		f.write(tweet + '\n')
 	f.close()
